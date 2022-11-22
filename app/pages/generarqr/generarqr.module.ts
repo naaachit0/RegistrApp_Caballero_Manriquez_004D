28340cb6d530dfa897ac0576a64e4a4b52d663e9
naaachit0 <80697125+naaachit0@users.noreply.github.com>
2022-11-21 22:10:33 -0400

    Experiencia 3

--- app/pages/generarqr/generarqr.module.ts
+++ app/pages/generarqr/generarqr.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { GenerarqrPageRoutingModule } from './generarqr-routing.module';
-
 import { GenerarqrPage } from './generarqr.page';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    QRCodeModule,
     GenerarqrPageRoutingModule
   ],
   declarations: [GenerarqrPage]
--- app/pages/generarqr/generarqr.page.ts
+++ app/pages/generarqr/generarqr.page.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerarqrPage implements OnInit {
 
+  qrCodeString = 'Hola Mundo';
+  scannedResult:any;
+
   constructor() { }
+
+  usuario={
+    nom:'',
+  }
+
+  generateScan(){
+    this.qrCodeString = this.usuario.nom;
+
+  }
+
+  verScan(){
+    this.scannedResult=this.qrCodeString;
+  }
 
   ngOnInit() {
   }
